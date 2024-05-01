955677e3547b05d4cecc247627a94cb34324cacd
Joseph O'Leary <oleary.joseph@outlook.com>
2024-05-01 20:46:42 +0200

    refactor

--- src/galaxybox/modules/emerge/statistics.py
+++ src/galaxybox/modules/emerge/statistics.py
@@ -1,15 +1,18 @@
 """Classes for handling Emerge data."""
+
+import os
+
 from galaxybox.data.emerge_io import read_statistics
 from galaxybox.visualization.emerge_plot import (
-    plot_stats_csfrd,
     plot_stats_clustering,
+    plot_stats_csfrd,
     plot_stats_fq,
     plot_stats_smf,
     plot_stats_ssfr,
 )
-import os
 
-class statistics:
+
+class EmergeStatistics:
     """Class for Emerge `statistics.h5` file."""
 
     def __init__(self, statistics_path, add_attrs=None):
@@ -21,49 +24,53 @@ class statistics:
             Path to the `statsitics.h5` file
         add_attrs : dict, optional
             A dictionary of additonal attributes to be attach to the class, by default None
-        """
 
+        """
         print(statistics_path)
         self.statistics_path = statistics_path
         fdata = read_statistics(self.statistics_path)
         fdatakeys = [key for key in fdata.keys()]
-        for i, k in enumerate(fdatakeys):
+        for k in fdatakeys:
             setattr(self, k, fdata[k])
         if add_attrs:
-            for i, k in enumerate(add_attrs.keys()):
+            for k in add_attrs.keys():
                 setattr(self, k, add_attrs[k])
 
     @classmethod
-    def from_universe(cls, Universe):
-        """Initiate `statistics` within the `Universe` class
+    def from_universe(cls, universe):
+        """Initiate `statistics` within the `Universe` class.
 
         Parameters
         ----------
-        Universe : `galaxybox.Universe`
+        universe : `galaxybox.Universe`
             The outer universe class used to organize this `statistics` class
 
         Returns
         -------
         galaxybox.statistics
             statistics set from the universe
+
         """
         add = {
-            "out_dir": Universe.out_dir,
-            "fig_dir": Universe.fig_dir,
-            "ModelName": Universe.ModelName,
-            "cosmology": Universe.cosmology,
+            "out_dir": universe.out_dir,
+            "fig_dir": universe.fig_dir,
+            "ModelName": universe.ModelName,
+            "cosmology": universe.cosmology,
         }
-        return cls(os.path.join(Universe.out_dir, "statistics.h5"), add)
+        return cls(os.path.join(universe.out_dir, "statistics.h5"), add)
 
     def plot(self, obs=None, save=False):
-        """A wrapper function the for the `plot_stats` functions in `emerge_plot.py`.
+        """Wrap `plot_stats` functions in `emerge_plot.py`.
 
         Parameters
         ----------
         obs : string or list of strings, optional
-            which observables should be plotted. If none all observables are plotted, by default None.
+            which observables should be plotted. If none all observables are plotted, by default
+            None.
         save : bool, optional
-            If true observationgs will be saved as a pdf in the designated figure directory, by default False.
+            If true observationgs will be saved as a pdf in the designated figure directory, by
+            default False.
+
         """
         # TODO: not a fan of this method, clean up later.
 

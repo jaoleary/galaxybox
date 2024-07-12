de46cf1aa735e630f3f8fa49e61881542fb477c0
Joseph O'Leary <oleary.joseph@outlook.com>
2024-07-12 22:23:22 +0200

    general updates to tree plotting

--- src/galaxybox/visualization/tree.py
+++ src/galaxybox/visualization/tree.py
@@ -4,48 +4,95 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import ticker
+from matplotlib.axes import Axes
 
 
-class TreePlotter:
-    """Class for plotting trees."""
+class TemporalTreePlotter:
+    """A class for plotting temporal trees with matplotlib.
 
-    tree = None
-    pos = None
-    node = None
-    current_y = 0
+    This class assumes the trees have depth first indexing and are sorted by scale factor.
 
-    scale = 1
+    Parameters
+    ----------
+    min_scale : int, optional
+        The minimum scale factor to consider for plotting, by default 0.
+    max_mu : int, optional
+        The maximum mass ratio (mu) to consider for plotting, by default 10000.
+    plot_style : str, optional
+        The style of the plotted tree. Can be "centered" or "linear", by default "centered".
 
-    def __init__(self, min_scale: int = 0, max_mu: int = 10000, mode: str = "centered"):
-        """Initialize the TreePlotter object.
+    Attributes
+    ----------
+    min_scale : int
+        The minimum scale factor to consider for plotting.
+    max_mu : int
+        The maximum mass ratio (mu) to consider for plotting.
+    plot_style : str
+        The style of the plotted tree. Can be "centered" or "linear", by default "centered".
+    time_col : str
+        Column name for the time scale in the tree data.
+    coprog_id_str : str
+        Column name for the coprogenitor ID in the tree data.
+    desc_id_str : str
+        Column name for the descendant ID in the tree data.
+    mmp_str : str
+        Column name for the most massive progenitor flag in the tree data.
+    num_prog_str : str
+        Column name for the number of progenitors in the tree data.
+    mass_str : str
+        Column name for the stellar mass in the tree data.
+    tree : pandas.DataFrame or None
+        The tree data.
+    pos : pandas.DataFrame or None
+        The positions of nodes in the tree.
+    node : int or None
+        The current node being processed.
+    current_y : int
+        The current y position in the plot.
+    scale : int
+        The scale factor for plotting.
 
-        Parameters
-        ----------
-        min_scale : int, optional
-            The minimum scale factor, by default 0
-        max_mu : int, optional
-            The maximum mass ratio, by default 10000
-        mode : str, optional
-            The mode of the tree visualization, by default "centered"
+    """
 
-        """
+    def __init__(self, min_scale: int = 0, max_mu: float = 10000, plot_style: str = "centered"):
         self.min_scale = min_scale
         self.max_mu = max_mu
-        self.mode = mode
 
-    def __call__(self, x, ax=None):
+        if plot_style not in ["centered", "linear"]:
+            return ValueError("Invalid plot style. Must be 'centered' or 'linear'.")
+
+        self.plot_style = plot_style
+
+        # TODO: allow for custom column names / aliasing
+        # Strings for the tree columns
+        self.time_col: str = "Scale"
+        self.coprog_id_str: str = "Coprog_ID"
+        self.desc_id_str: str = "Desc_ID"
+        self.mmp_str: str = "MMP"
+        self.num_prog_str: str = "Num_prog"
+        self.mass_str: str = "Stellar_mass"
+
+        self.tree: pd.DataFrame | None = None
+        self.pos: pd.DataFrame | None = None
+        self.node: int | None = None
+        self.current_y: int = 0
+
+        self.scale: float = 1.0
+
+    def __call__(self, tree: pd.DataFrame, ax: Axes | None = None):
         """Call the Tree object as a function.
 
         Parameters
         ----------
-        x : Any
+        tree : pd.DataFrame
             The input data for the tree.
         ax : Optional[matplotlib.axes.Axes], optional
             The matplotlib axes to render the tree on. If not provided, a new axes will be created.
 
         Returns
         -------
-        None
+        tuple(fig, ax)
+            The matplotlib figure and axes.
 
         Raises
         ------
@@ -63,29 +110,41 @@ class TreePlotter:
         >>> tree(data)
 
         """
-        self._init_tree(x)
+        self._init_tree(tree)
 
         if ax is None:
             self._init_ax()
+            fig = self.fig
         else:
             self.ax = ax
+            fig = None
 
         self.set_pos()
         self.render()
         self._reset()
+        return fig, ax
 
     def _reset(self):
+        """Reset the internal state of the plotter."""
         self.tree = None
         self.pos = None
         self.node = None
         self.current_y = 0
 
-    def _init_tree(self, x):
-        self.tree = x
+    def _init_tree(self, tree: pd.DataFrame):
+        """Initialize the tree with the given DataFrame.
+
+        Parameters
+        ----------
+        tree : pd.DataFrame
+            The input data for the tree.
+
+        """
+        self.tree = tree
 
         self.pos = pd.DataFrame(index=self.tree.index.values, columns=["x", "y"])
         self.pos.index.name = "id"
-        self.pos["x"] = self.tree.scale.values
+        self.pos["x"] = self.tree[self.time_col].values
 
         self.node = self.tree.index[0]
 
@@ -93,26 +152,17 @@ class TreePlotter:
         """Set the position of the current node."""
         # set the position of current node
         self.pos.loc[self.node, "y"] = self.current_y
-        self.scale = self.tree.loc[self.node].scale
-        # self.pos.loc[self.node, 'y'] = self.ypos[self.scale]
+        self.scale = self.tree.loc[self.node, self.time_col]
 
         # get the ID of the most massive progenitor
         immp = 0
-        if self.tree.loc[self.node]["num_prog"] > 0:
+        if self.tree.loc[self.node, self.num_prog_str] > 0:
             immp = self.tree.loc[
-                (self.tree.desc_id == self.node) & (self.tree.mmp == 1)
+                (self.tree[self.desc_id_str] == self.node) & (self.tree[self.mmp_str] == 1)
             ].index.values[0]
 
         # doing a mass ratio check on coprogs
-        icoprog = int(self.tree.loc[self.node]["Next_coprogenitor_depthfirst_ID"])
-        if icoprog > 0:
-            icoprog = self.tree.loc[self.tree.Depth_first_ID == icoprog].index.values[0]
-            dat_c = self.tree.loc[icoprog]
-            did_c = int(dat_c.desc_id)
-            main_prog = self.tree.loc[(self.tree.desc_id == did_c) & (self.tree.mmp == 1)]
-            mu = 10 ** (float(main_prog.mvir) - dat_c.mvir)
-            if mu > self.max_mu:
-                icoprog = 0
+        icoprog = self._get_coprog_id()
 
         if self.scale > self.min_scale:
             # walk the main branch first
@@ -122,36 +172,72 @@ class TreePlotter:
             # walk the coprogenitors
             if icoprog > 0:
                 self.current_y += 1
-                # self.ypos[self.scale] += 1
                 self.node = icoprog
                 self.set_pos()
 
-            if self.mode == "centered":
+            if self.plot_style == "centered":
                 self._update_desc_pos()
 
-    def _get_mmp_id(self, idx):
-        if self.tree.loc[idx]["num_prog"] > 0:
-            return self.tree.loc[(self.tree.desc_id == idx) & (self.tree.mmp == 1)].index.values[0]
+    def _get_coprog_id(self):
+        """Get the ID of the coprogenitor node.
+
+        Returns
+        -------
+        int
+            The ID of the coprogenitor node.
+
+        """
+        icoprog = int(self.tree.loc[self.node][self.coprog_id_str])
+        if icoprog > 0:
+            icoprog = self.tree.loc[self.tree.index == icoprog].index.values[0]
+            dat_c = self.tree.loc[icoprog]
+            did_c = int(dat_c[self.desc_id_str])
+            main_prog = self.tree.loc[
+                (self.tree[self.desc_id_str] == did_c) & (self.tree[self.mmp_str] == 1)
+            ]
+            mu = 10 ** (main_prog[self.mass_str].values[0] - dat_c[self.mass_str])
+            if mu > self.max_mu:
+                icoprog = 0
+        return icoprog
+
+    def _get_mmp_id(self, idx: int):
+        """Get the ID of the most massive progenitor (MMP).
+
+        Parameters
+        ----------
+        idx : int
+            The index of the current node.
+
+        Returns
+        -------
+        int
+            The ID of the MMP.
+
+        """
+        if self.tree.loc[idx, self.num_prog_str] > 0:
+            return self.tree.loc[
+                (self.tree[self.desc_id_str] == idx) & (self.tree[self.mmp_str] == 1)
+            ].index.values[0]
 
     def _update_desc_pos(self):
-        desc_id = self.tree.loc[self.node].desc_id
+        """Update the position of descendant nodes."""
+        desc_id = self.tree.loc[self.node, self.desc_id_str]
         max_depth = 1
         depth = 0
 
         while (desc_id > 0) and (depth < max_depth):
-            if self.tree.loc[self.node].mmp == 0:
+            if self.tree.loc[self.node, self.mmp_str] == 0:
                 immp = self._get_mmp_id(desc_id)
                 mmp_y = self.pos.loc[immp, "y"]
                 now_y = self.pos.loc[self.node, "y"]
 
                 self.pos.loc[desc_id, "y"] = (now_y + mmp_y) / 2
-                # self.pos.loc[desc_id, 'y'] += 0.5
                 depth += 1
             else:
                 self.pos.loc[desc_id, "y"] = self.pos.loc[self.node, "y"]
 
             self.node = desc_id
-            desc_id = self.tree.loc[self.node].desc_id
+            desc_id = self.tree.loc[self.node, self.desc_id_str]
 
     def render(self):
         """Render the tree visualization."""
@@ -159,27 +245,28 @@ class TreePlotter:
         # so we need to ignore those
         valid_idx = ~self.pos["y"].isna()
         for idx in self.pos[valid_idx].index.values:
-            if self.tree.loc[idx].desc_id == 0:
+            if self.tree.loc[idx, self.desc_id_str] == 0:
                 continue
 
-            desc_id = self.tree.loc[idx].desc_id
-            x = [self.pos.loc[idx]["x"], self.pos.loc[desc_id]["x"]]
-            y = [self.pos.loc[idx]["y"], self.pos.loc[desc_id]["y"]]
+            desc_id = self.tree.loc[idx, self.desc_id_str]
+            x = [self.pos.loc[idx, "x"], self.pos.loc[desc_id, "x"]]
+            y = [self.pos.loc[idx, "y"], self.pos.loc[desc_id, "y"]]
             self.ax.plot(x, y, color="k", zorder=0)
 
         self.ax.scatter(
-            self.pos.loc[valid_idx]["x"],
-            self.pos.loc[valid_idx]["y"],
-            c=self.tree.loc[valid_idx]["mvir"],
+            self.pos.loc[valid_idx, "x"],
+            self.pos.loc[valid_idx, "y"],
+            c=self.tree.loc[valid_idx, self.mass_str],
             zorder=10,
             vmin=9,
-            vmax=self.tree.mvir.max(),
+            vmax=self.tree[self.mass_str].max(),
             cmap=plt.cm.jet,
         )
 
     def _init_ax(self):
-        vmin = 9
-        vmax = self.tree.mvir.max()
+        """Initialize the matplotlib axes for the plot."""
+        vmin = 7
+        vmax = self.tree[self.mass_str].max()
         self.fig, self.ax = plt.subplots(figsize=(20, 10))
         self.ax.xaxis.set_major_locator(ticker.FixedLocator(np.arange(0, 1.1, 0.1)))
         self.ax.xaxis.set_minor_locator(ticker.FixedLocator(np.arange(0, 1, 0.025)))
@@ -188,8 +275,8 @@ class TreePlotter:
         self.ax.set_yticks([])
 
         sm = plt.cm.ScalarMappable(cmap=plt.cm.jet, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-        self.cbar = plt.colorbar(sm, pad=0.01, aspect=30)
+        self.cbar = plt.colorbar(sm, ax=self.ax, pad=0.01, aspect=30)
         self.cbar.ax.xaxis.set_label_position("top")
         self.cbar.ax.xaxis.set_ticks_position("top")
         self.cbar.ax.minorticks_on()
-        self.cbar.set_label("$\log_{10}(M_{\\mathrm{h}}/M_{\odot})$", fontsize=18)
+        self.cbar.set_label("$\log_{10}(M_{\\mathrm{*}}/M_{\odot})$", fontsize=18)
